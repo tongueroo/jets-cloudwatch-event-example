6ad8412de792e4917e950518cddb30691d4ae307
Tung Nguyen <tongueroo@gmail.com>
2019-02-18 05:13:20 +0000

    add security job

---
+++ app/jobs/security_job.rb
@@ -0,0 +1,13 @@
+class SecurityJob < ApplicationJob
+  event_pattern(
+    source: ["aws.ec2"],
+    detail_type: ["EC2 Instance State-change Notification"],
+    detail: {
+      state: ["stopping"],
+    }
+  )
+  def instance_stopping
+    puts "event #{JSON.dump(event)}" # event is available
+    # logic goes here
+  end
+end
